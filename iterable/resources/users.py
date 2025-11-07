2bd34c12f977b6840336d110bdb78178970a33c9
akshitkrnagpal <akshitkrnagpal@gmail.com>
2025-11-07 09:39:06 +0400

    Refactor user deletion method to use userId instead of email. Updated method documentation to reflect changes and added a warning about data loss upon deletion.

--- iterable/resources/users.py
+++ iterable/resources/users.py
@@ -22,14 +22,19 @@ class UsersResource(BaseResource):
 
         return self.client.post("users/bulkUpdate", data=data)
 
-    def delete(self, email):
+    def delete(self, user_id):
         """
-        Delete a user by email.
+        Delete a user by userId.
 
-        :param email: user's email address
+        WARNING: This endpoint completely deletes the specified user profile,
+        including subscription settings and event history. If the user is ever
+        re-added, their profile will be empty and subscribed to everything by
+        default. Deleting a user removes their historic campaign metrics, too.
+
+        :param user_id: user's ID
         :return: API response
         """
-        return self.client.post("users/delete", data={"email": email})
+        return self.client.delete(f"users/byUserId/{user_id}")
 
     def get(self, email):
         """
